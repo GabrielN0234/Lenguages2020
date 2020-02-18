14cf9cf33355d722cfaeaf3094fdaf1b7e511ee8
GabrielN0234 <49532310+GabrielN0234@users.noreply.github.com>
2020-02-18 13:34:48 -0600

    Add files via upload

---
+++ APISuport(.net)/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace APISupport.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ APISuport(.net)/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APISupport.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
